01adec369b18c72f0660d11435ba432d6edbb421
NamJungGu <nowage@gmail.com>
2019-04-17 09:02:55 +0900

    README.md added

---
+++ README.sh
@@ -0,0 +1,28 @@
+# Usage
+```
+git clone https://github.com/Finfra/jenkins_quickstart
+cd jenkins_quickstart
+vagrant up
+```
+
+# For windows User
+* after 'vagrant up', u must do network setting 
+## jenkins1
+click setting → Network →  Adapter1 → Port forwarding →  "+" button →  Rule1(Host IP:127.0.0.1,HostPort:80,GUEST IP:(not set),GuestPort:8080) →  OK
+
+## jm1(Tomcat Server)
+click setting → Network →  Adapter1 → Port forwarding →  "+" button →  Rule1(Host IP:127.0.0.1,HostPort:8080,GUEST IP:(not set),GuestPort:8080) →  OK
+
+## jm2(Artifactory Server)
+click setting → Network →  Adapter1 → Port forwarding →  "+" button →  Rule1(Host IP:127.0.0.1,HostPort:81,GUEST IP:(not set),GuestPort:8081) →  OK
+
+
+# for Ubuntu/Mac User
+* after 'vagrant up', u must do network setting
+System Preference →  Network →  "+" button →  Network CIDR: 172.22.101.0/24 →  OK
+exec cute ". check.sh"
+
+
+
+# if u has reboot, do this
+vagrant ssh jm2 -c "sudo docker start artifactory"
