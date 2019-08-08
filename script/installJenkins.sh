1cbcb929689c2fcdf4d5689d085679e62d95af44
NamJungGu <nowage@gmail.com>
2019-08-08 09:59:09 +0900

    Update installJenkins.sh

--- script/installJenkins.sh
+++ script/installJenkins.sh
@@ -3,7 +3,7 @@ apt-get -y install jenkins
 apt-get -y install maven
 apath=$(dirname $(ls -als $(ls -als $(which mvn) |awk '{print $NF}')|awk '{print $NF}'))
 echo export M2_HOME=${apath%/*} >> /etc/bash.bashrc
-echo export VAMEN_HOME=${apath%/*} >> /etc/bash.bashrc
+echo export MAVEN_HOME=${apath%/*} >> /etc/bash.bashrc
 echo export PATH=\$PATH:$apath    >> /etc/bash.bashrc
 tail /etc/bash.bashrc
 . /etc/bash.bashrc
